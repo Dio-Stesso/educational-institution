d96a9fda094c999e4dffa906dab55e2f87a13533
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2022-11-21 16:08:31 +0200

    Added constructor annotations.

--- src/main/java/com/education/institution/model/Student.java
+++ src/main/java/com/education/institution/model/Student.java
@@ -10,11 +10,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "students")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
