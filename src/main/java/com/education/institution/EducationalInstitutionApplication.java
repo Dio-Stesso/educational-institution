7b8b1a85fbb9538344a16f93b5f8baba3592ea59
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2022-11-21 13:06:31 +0200

    Project initial commit.

---
+++ src/main/java/com/education/institution/EducationalInstitutionApplication.java
@@ -0,0 +1,12 @@
+package com.education.institution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalInstitutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalInstitutionApplication.class, args);
+    }
+}
---
+++ src/test/java/com/education/institution/EducationalInstitutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.institution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalInstitutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
